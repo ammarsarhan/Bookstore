142c5ae0f2e4a8b995f591cf3eb43350bba34254
ammarsarhan <ammarsarhan06@gmail.com>
2024-05-12 12:14:55 +0300

    Created Blueprint For Book Object

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template
 import requests
 import json
 
+from items import Book
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     res = requests.get("http://127.0.0.1:5000/data/books")
-    books = res.json()
+    data = res.json()
+
+    books = []
+
+    for item in data:
+        book = Book(item["title"], item["author"], item["price"], item["ISBN"], item["genre"], item["pages"])
+        books.append(book)
+
     return render_template("index.html", books = books)
 
 @app.route('/dashboard')
